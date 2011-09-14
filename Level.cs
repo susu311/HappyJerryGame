ecb4cedac28e7162a9c18f652cc9ec2d84b1abe2
Su Su Thant <ssct311@gmail.com>
2011-09-14 23:16:23 +0100

    first commit

---
+++ Level.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace HappyJerryGame
+{
+    public class Level
+    {
+        public class character
+        {
+            public string name;
+            public Vector2 position;
+            public string greeting;
+            public float scale;
+            public string textureAsset;
+            int correctChoice;
+            [XmlArrayItem]
+            public string[] choice;
+            [XmlIgnore]
+            public BoundingSphere bound;
+            [XmlIgnore]
+
+            public Texture2D sprite;
+        }
+        //other classes could be added for collectable objects / physical layouts / everything you might need to define a level
+
+        [XmlArrayItem]
+        public character[] allCharacters;
+    }
+    
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HappyJerryGame")]
+[assembly: AssemblyProduct("HappyJerryGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("University of Nottingham")]
+
+[assembly: AssemblyCopyright("Copyright © University of Nottingham 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02b5a702-d037-4b17-a5f3-6ff3f4246c59")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
